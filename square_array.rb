f500fe64e67bd0022f0cc4d0b90b05585f535d60
Jesse Wenger <wengerj@gmail.com>
2021-03-03 01:49:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
 #def square_array(array)
 #  # your code here
 #  square_array = Array.new
-#  array.each do |item|
-#    square_array << item ** 2
+#  array.each do |i|
+#    square_array << i ** 2
 #  end
 #  square_array
 #end
 
+def square_array.collect{|i| i ** 2}
+end
